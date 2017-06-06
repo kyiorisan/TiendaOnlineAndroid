e1ee5235baca532a4103ff57075ca5e0add38341
kyiorisan <kikingsan@gmail.com>
2017-06-06 17:05:52 -0500

    Initial commit

---
+++ app/src/main/java/net/ddns/tiendaonline/tiendaonline/Models/EntityModels/Buyer.java
@@ -0,0 +1,9 @@
+package net.ddns.tiendaonline.tiendaonline.Models.EntityModels;
+
+
+/**
+ * Created by Dante on 06/06/2017.
+ */
+
+public class Buyer extends User {
+}
---
+++ app/src/main/java/net/ddns/tiendaonline/tiendaonline/Models/EntityModels/Category.java
@@ -0,0 +1,8 @@
+package net.ddns.tiendaonline.tiendaonline.Models.EntityModels;
+
+/**
+ * Created by Dante on 06/06/2017.
+ */
+
+public class Category {
+}
